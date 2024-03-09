848a5e31446403a2ecee1b80dbbf3ad1e0469aad
Oguz-Guzel <44399848+Oguz-Guzel@users.noreply.github.com>
2024-03-09 09:35:55 +0100

    update script after signal stacking

--- scripts/plot_DL.sh
+++ scripts/plot_DL.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cp scripts/controlPlotter_DL.tex ${1}/plots
-cd ${1}/plots
+cp scripts/controlPlotter_DL.tex ${1}/plots_full
+cd ${1}/plots_full
 pdflatex controlPlotter_DL.tex
 cd -
-mv ${1}/plots/controlPlotter_DL.pdf ${1}.pdf
+mv ${1}/plots_full/controlPlotter_DL.pdf ${1}.pdf
 # pdflatex yields.tex
 # cd ../..
