68588bc4391296564d736334f53cfa7f2008333b
Oguz Guzel <44399848+Oguz-Guzel@users.noreply.github.com>
2024-12-04 16:52:43 +0100

    remove tau veto and SL channel from selections.py

--- python/selections.py
+++ python/selections.py
@@ -1,16 +1,9 @@
 from bamboo import treefunctions as op
-from bamboo.plots import CategorizedSelection
-
 from scalefactors import ScaleFactors as sf
 
-# common variables for DL and SL channels
 
-Zmass = 91.1876 # GeV
+Zmass = 91.1876  # GeV
 
-
-def tau_h_veto(taus) -> bool:
-    "Veto events with hadronic taus."
-    return op.rng_len(taus) == 0
 
 def lowMllCut(dileptons) -> bool:
     " Minimum dilepton invariant mass cut, that's 12 GeV."
@@ -22,8 +15,6 @@ def outZ(dileptons) -> bool:
     "Reject events with same-flavoured dilepton mass around Z peak."
     return op.NOT(op.rng_any(
         dileptons, lambda dilep: op.invariant_mass(dilep[0].p4, dilep[1].p4) > Zmass - 10.))
-
-# end common variables
 
 
 def makeDLSelection(self, sel):
@@ -75,20 +66,17 @@ def makeDLSelection(self, sel):
     emuTightPair = op.combine((self.tightElectrons, self.tightMuons),
                               N=2, pred=lambda el, mu: el.charge != mu.charge)
 
-    # the actual leptons that will be used for the analysis
+    # the actual lepton pairs that will be used for the analysis
     self.firstElTightPair = elTightPair[0]
     self.firstMuTightPair = muTightPair[0]
     self.firstEmuTightPair = emuTightPair[0]
-    
-    # tau veto
-    tauVetoSel = sel.refine('tauVetoSel', cut=tau_h_veto(self.cleanedTaus))
 
     # minimum pT cut : at least one lepton pair with leading lepton above 25 GeV
-    elPairMinPtSel = tauVetoSel.refine(
+    elPairMinPtSel = sel.refine(
         'elPairMinPtSel', cut=[ptCutSameFlavourPair(self.firstElTightPair)])
-    muPairMinPtSel = tauVetoSel.refine(
+    muPairMinPtSel = sel.refine(
         'muPairMinPtSel', cut=[ptCutSameFlavourPair(self.firstMuTightPair)])
-    emuPairMinPtSel = tauVetoSel.refine(
+    emuPairMinPtSel = sel.refine(
         'emuPairMinPtSel', cut=[ptCutDifferentFlavourPair(self.firstEmuTightPair)])
 
     # low Mll cut : reject events with dilepton mass below 12 GeV
@@ -122,23 +110,23 @@ def makeDLSelection(self, sel):
         op.rng_len(emuTightPair) == 1,
     )])
 
-    # try applying lepton sf here 
-    # add muoniso sf
-    # add trigger sf 
-
-    # muonSF
+    # di-muon channel SF
     muPairMultiplicitySel = sf.muonSF(self, muPairMultiplicitySel)
 
-    # electronSF
+    # di-electron channel SF
     elPairMultiplicitySel = sf.electronSF(self, elPairMultiplicitySel)
 
-    # e-mu SF
+    # e-mu channel SF
     emuPairMultiplicitySel = sf.muonSF(self, emuPairMultiplicitySel)
     emuPairMultiplicitySel = sf.electronSF(self, emuPairMultiplicitySel)
 
-    DL_boosted_pre_ee = elPairMultiplicitySel.refine('DL_boosted_pre_ee', cut=op.c_bool(1))
-    DL_boosted_pre_mumu = muPairMultiplicitySel.refine('DL_boosted_pre_mumu', cut=op.c_bool(1))
-    DL_boosted_pre_emu = emuPairMultiplicitySel.refine('DL_boosted_pre_emu', cut=op.c_bool(1))
+    # boosted pre-final state selections for btag reweighting
+    DL_boosted_pre_ee = elPairMultiplicitySel.refine(
+        'DL_boosted_pre_ee', cut=op.c_bool(1))
+    DL_boosted_pre_mumu = muPairMultiplicitySel.refine(
+        'DL_boosted_pre_mumu', cut=op.c_bool(1))
+    DL_boosted_pre_emu = emuPairMultiplicitySel.refine(
+        'DL_boosted_pre_emu', cut=op.c_bool(1))
 
     # boosted -> at least one b-tagged ak8 jet
     DL_boosted_ee = DL_boosted_pre_ee.refine(
@@ -148,9 +136,13 @@ def makeDLSelection(self, sel):
     DL_boosted_emu = DL_boosted_pre_emu.refine(
         'DL_boosted_emu', cut=(op.rng_len(self.ak8BJets) >= 1))
 
-    DL_resolved_pre_ee = elPairMultiplicitySel.refine('DL_resolved_pre_ee', cut=[op.rng_len(self.ak4Jets) >= 2])
-    DL_resolved_pre_mumu = muPairMultiplicitySel.refine('DL_resolved_pre_mumu', cut=[op.rng_len(self.ak4Jets) >= 2])
-    DL_resolved_pre_emu = emuPairMultiplicitySel.refine('DL_resolved_pre_emu', cut=[op.rng_len(self.ak4Jets) >= 2])
+    # resolved pre-final state selections for btag reweighting
+    DL_resolved_pre_ee = elPairMultiplicitySel.refine(
+        'DL_resolved_pre_ee', cut=[op.rng_len(self.ak4Jets) >= 2])
+    DL_resolved_pre_mumu = muPairMultiplicitySel.refine(
+        'DL_resolved_pre_mumu', cut=[op.rng_len(self.ak4Jets) >= 2])
+    DL_resolved_pre_emu = emuPairMultiplicitySel.refine(
+        'DL_resolved_pre_emu', cut=[op.rng_len(self.ak4Jets) >= 2])
 
     # resolved -> and at least two ak4 jets with 1 or at least 2 b-tagged jets and no ak8 jets
     DL_resolved_1b_ee = DL_resolved_pre_ee.refine(
@@ -195,99 +187,11 @@ def makeDLSelection(self, sel):
             op.rng_len(self.ak8BJets) == 0))
     )
 
-    pre_final_state_sels = [DL_boosted_pre_ee, DL_boosted_pre_mumu, DL_boosted_pre_emu, DL_resolved_pre_ee, DL_resolved_pre_mumu, DL_resolved_pre_emu]
+    pre_final_state_sels = [DL_boosted_pre_ee, DL_boosted_pre_mumu, DL_boosted_pre_emu,
+                            DL_resolved_pre_ee, DL_resolved_pre_mumu, DL_resolved_pre_emu]
 
     DL_selections = [DL_boosted_ee, DL_boosted_mumu, DL_boosted_emu,
                      DL_resolved_1b_ee, DL_resolved_1b_mumu, DL_resolved_1b_emu,
                      DL_resolved_2b_ee, DL_resolved_2b_mumu, DL_resolved_2b_emu]
 
     return DL_selections, pre_final_state_sels
-
-
-def makeSLSelection(self, sel):
-    """Creates a list of selection objects for the Single Lepton final state.
-
-    Args:
-        sel: the selection to be used as a base for the Single Lepton selections.
-
-    Returns:
-        A list of selection objects for the Single Lepton analysis.
-    """
-
-    def elPtCut(lep): return lep[0].pt > 15.0
-
-    def muPtCut(lep): return lep[0].pt > 15.0
-
-    # OS loose lepton pairs of same type to be vetoed around Z peak
-    elLoosePair = op.combine(
-        self.clElectrons, N=2, pred=lambda lep1, lep2: lep1.charge != lep2.charge)
-    muLoosePair = op.combine(
-        self.preMuons, N=2, pred=lambda lep1, lep2: lep1.charge != lep2.charge)
-    emuLoosePair = op.combine(
-        (self.clElectrons, self.preMuons), N=2, pred=lambda el, mu: el.charge != mu.charge)
-
-    # Z-veto : reject events with dileptons of same type with mass around Z peak
-    outZCut = op.AND(outZ(elLoosePair), outZ(muLoosePair))
-
-    # low Mll cut : reject events with dilepton mass below 12 GeV
-    mllCut = op.AND(lowMllCut(elLoosePair), lowMllCut(
-        muLoosePair), lowMllCut(emuLoosePair))
-
-    outZelPairSel = sel.refine('OSoutZsel', cut=op.AND(
-        mllCut, outZCut, tau_h_veto(self.cleanedTaus)))
-
-    SL_resolved_pre = outZelPairSel.refine('SL_resolved_pre', cut=[
-        op.rng_len(self.ak4Jets) >= 3,
-        op.rng_len(self.ak4BJets) >= 1,
-        op.rng_len(self.ak8BJets) == 0])
-
-    SL_resolved_1b_pre = SL_resolved_pre.refine('SL_resolved_1b_pre', cut=[
-        op.rng_len(self.ak4BJets) == 1])
-
-    SL_resolved_2b_pre = SL_resolved_pre.refine('SL_resolved_2b_pre', cut=[
-        op.rng_len(self.ak4BJets) > 1])
-
-    SL_resolved_1b_e = SL_resolved_1b_pre.refine('SL_resolved_1b_e', cut=[
-        elPtCut(self.tightElectrons),
-        op.rng_len(self.tightElectrons) == 1,
-        op.rng_len(self.tightMuons) == 0])
-
-    SL_resolved_2b_e = SL_resolved_2b_pre.refine('SL_resolved_2b_e', cut=[
-        elPtCut(self.tightElectrons),
-        op.rng_len(self.tightElectrons) == 1,
-        op.rng_len(self.tightMuons) == 0])
-
-    SL_resolved_1b_mu = SL_resolved_1b_pre.refine('SL_resolved_1b_mu', cut=[
-        muPtCut(self.tightMuons),
-        op.rng_len(self.tightElectrons) == 0,
-        op.rng_len(self.tightMuons) == 1])
-
-    SL_resolved_2b_mu = SL_resolved_2b_pre.refine('SL_resolved_2b_mu', cut=[
-        muPtCut(self.tightMuons),
-        op.rng_len(self.tightElectrons) == 0,
-        op.rng_len(self.tightMuons) == 1])
-
-    SL_boosted = outZelPairSel.refine('SL_boosted', cut=[
-        op.rng_len(self.ak8BJets) >= 1,
-        op.rng_len(self.ak4JetsCleanedFromAk8b) >= 1])
-
-    SL_boosted_e = SL_boosted.refine('SL_boosted_e', cut=[
-        elPtCut(self.tightElectrons),
-        op.rng_len(self.tightElectrons) == 1,
-        op.rng_len(self.tightMuons) == 0])
-
-    SL_boosted_mu = SL_boosted.refine('SL_boosted_mu', cut=[
-        muPtCut(self.tightMuons),
-        op.rng_len(self.tightMuons) == 1,
-        op.rng_len(self.tightElectrons) == 0])
-
-    SL_e = CategorizedSelection("SL_e", categories={
-        "resolved_1b": (SL_resolved_1b_e, lambda event: True),
-        "resolved_2b": (SL_resolved_2b_e, lambda event: True),
-        "boosted": (SL_boosted_e, lambda event: True)
-    })
-
-    SL_selections = [SL_resolved_pre, SL_resolved_1b_e, SL_resolved_2b_e,
-                     SL_resolved_1b_mu, SL_resolved_2b_mu, SL_boosted, SL_boosted_e, SL_boosted_mu, SL_e]
-
-    return SL_selections
