ef67c09b2b9f276addaa2f1297b3164b88c06755
Oguz Guzel <44399848+Oguz-Guzel@users.noreply.github.com>
2025-01-29 17:48:54 +0100

    refactor: remove unused plot_SL.sh script and update controlPlotter_DL.tex titles for clarity

--- scripts/plot_SL.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cp scripts/controlPlotter_SL.tex ${1}/plots_full
-cd ${1}/plots_full
-pdflatex controlPlotter_SL.tex
-cd -
-mv ${1}/plots_full/controlPlotter_SL.pdf ${1}.pdf
-# pdflatex yields.tex
-# cd ../..
